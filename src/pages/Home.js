ec2943cd58902892b3a2f76c4ef901478d0cd5db
Pratyush Mishra <pratyush.m0102@gmail.com>
2025-10-06 19:19:31 +0530

    debug the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -148,7 +148,7 @@ export default function Home() {
                 <div
                   className="absolute inset-0 bg-cover bg-center"
                   style={{
-                    backgroundImage: `url('https://images.unsplash.com/photo-1579203643790-252750e391ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`
+                    backgroundImage: `url('https://images.unsplash.com/photo-1656828059867-3fb503eb2214?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxuYWluaXRhbCUyMGxha2UlMjBzdW5zZXQlMjBtb3VudGFpbnN8ZW58MXx8fHwxNzU3NjE2OTg3fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral')`
                   }}
                 />
                 <div className="absolute inset-0  bg-opacity-30" /> {/* Changed to bg-black for consistency */}
@@ -170,11 +170,10 @@ export default function Home() {
                 <div
                   className="absolute inset-0 bg-cover bg-center"
                   style={{
-                     // Changed the image URL as above
-                    backgroundImage: `url('https://images.unsplash.com/photo-1547844111-c967929d10e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`
+                    backgroundImage: `url('https://images.unsplash.com/photo-1683973200791-47539048cf63?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxiaGltdGFsJTIwbGFrZSUyMHV0dGFyYWtoYW5kfGVufDF8fHx8MTc1NzYxNjk4OHww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral')`
                   }}
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-30" /> {/* Changed to bg-black for consistency */}
+                <div className="absolute inset-0  bg-opacity-30" /> 
                 <div className="absolute inset-0 flex items-center justify-center">
                   <div className="text-center text-white">
                     <h3 className="text-2xl font-bold mb-2">Explore Lakes</h3>
